590cc9f091c6189bb799d661487a5df2e953d0b0
Brisa-Codes <brisamukunde1@gmail.com>
2022-06-01 00:04:22 +0300

    update: signup routes

--- routes/signup.js
+++ routes/signup.js
@@ -1,25 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const Signup = require('../models/signupModel');
 
 router.get('/signup', (req, res) => {
     res.render('signup');
-});
-
-router.post('/signup', (req, res) => {
-    const newSignup = new Signup({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-
-    newSignup.save((err) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        res.redirect('/signin');
-    });
 });
 
 module.exports = router;
