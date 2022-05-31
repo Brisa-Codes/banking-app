c439338a0091097b749936289a80c2738077953a
Brisa-Codes <brisamukunde1@gmail.com>
2022-05-31 23:10:57 +0300

    update: models

---
+++ models/signupModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+// sign up schema
+const signupSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+signupSchema.plugin(passportLocalMongoose, {
+    usernameField: 'email'
+});
+
+// connect to mongoose
+const Signup = module.exports = mongoose.model('Signup', signupSchema);
