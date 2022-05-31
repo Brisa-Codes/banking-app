af804f9879497d9cf95763d3fb259791f86422d7
Brisa-Codes <brisamukunde1@gmail.com>
2022-05-31 23:13:09 +0300

    update: server

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
 
+// db configuration
+const config = require('./config/database');
 
 // express
 const app = express();
@@ -19,11 +21,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// database setup
+mongoose.connect('mongodb://localhost/banking-app');
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+db.on('error', (err) => {
+    console.log(err);
+});
+
 // importing routes
 const home = require('./routes/home');
+const signup = require('./routes/signup');
+const Signup = require('./models/signupModel');
 
 // routes
 app.use('/', home);
+app.use('/signup', signup);
 
 // server port
 app.listen(3000, () => {
