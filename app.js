a2b7526476f2479c695349c80311acc45f4fb67a
Brisa-Codes <brisamukunde1@gmail.com>
2022-05-31 19:45:23 +0300

    update: server file

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ const path = require('path');
 // express
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// middleware setup
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// importing routes
+const home = require('./routes/home');
+
+// routes
+app.use('/', home);
+
 // server port
 app.listen(3000, () => {
     console.log('listening on port 3000');
