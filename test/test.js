619769a961a0b26c276c08d20daf210bbfc58108
Brisa-Codes <brisamukunde1@gmail.com>
2022-06-07 16:30:49 +0300

    update: mocha-tests

---
+++ test/test.js
@@ -0,0 +1,42 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+const should = chai.should();
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('/GET test our APIs ', () => {
+
+    // sign up api test
+    it('it should GET signup', () => {
+        chai.request(app)
+            .get('/signup')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            });
+    });
+
+    // login api test
+    it('/GET should get Login', () => {
+        chai.request(app)
+        .get('/login')
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+        });
+    });
+
+    // test for dashboard api
+    it('/GET should get dashboard', () => {
+        chai.request(app)
+        .get('/dashboard')
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            done();
+        });
+    });
+    
+});
