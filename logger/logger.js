520e1dbd57f0c2d2b88713602ad370ea8119f462
Brisa-Codes <brisamukunde1@gmail.com>
2022-06-08 10:21:42 +0300

    update: logger file

--- logger/logger.js
+++ logger/logger.js
@@ -4,6 +4,7 @@ const logger = pino({
     transport: {
         target: "pino-pretty",
     options: {
+        colorize: true,
         translateTime: 'SYS:standard',
         ignore: "pid, hostname"
     },
